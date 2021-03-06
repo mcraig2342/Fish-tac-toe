0fc9c1d1ef03b5b52f61e0a9be34e0788a12b1ff
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-06 12:16:46 -0700

    Add id's to each square and add query selectors for each square

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ var currentBoard = [
   [0, 0, 0]
   [0, 0, 0]
 ]
+
+var square1 = document.getElementById("square1");
+var square2 = document.getElementById("square2");
+var square3 = document.getElementById("square3");
+var square4 = document.getElementById("square4");
+var square5 = document.getElementById("square5");
+var square6 = document.getElementById("square6");
+var square7 = document.getElementById("square7");
+var square8 = document.getElementById("square8");
+var square9 = document.getElementById("square9");
