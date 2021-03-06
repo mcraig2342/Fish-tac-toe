e94ad1a0a8c3fda97e73476f4ce2b33aa5c5276a
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-06 12:01:30 -0700

    Split check win method into 2 methods, one for each player, and refactored

--- src/game.js
+++ src/game.js
@@ -4,10 +4,11 @@ class Game {
     this.player2 = player2;
     this.turn = this.player1;
     this.totalMoves = 0;
-    this.gameBoard = [0,0,0
-                      0,0,0
-                      0,0,0]
-
+    this.gameBoard = [
+      [0, 0, 0]
+      [0, 0, 0]
+      [0, 0, 0]
+    ]
   }
 
   changeTurn() {
@@ -22,26 +23,48 @@ class Game {
     this.gameBoard = currentBoard;
   }
 
-  checkWin(winningBoardsPlayer1, winningBoardsPlayer2) {
-    if (winningBoardsPlayer1.includes(this.gameBoard)) {
-      this.player1.wins++
-      return "Player 1 wins"
-    } else if (winningBoardsPlayer2.includes(this.gameBoard)) {
-      this.player2.wins++
-      return "Player 2 wins"
-    }
-  }
-    checkCatsGame() {
-      if (this.totalMoves >= 9) {
-        return "Cats game"
+  checkWinPlayer1() {
+    for (var i = 0; i < this.gameBoard.length; i++) {
+      if (this.gameBoard[i][0] === 1 && this.gameBoard[i][1] === 1 && this.gameBoard[i][2] === 1) {
+        return "Player 1 wins";
+      } else if (this.gameBoard[0][i] === 1 && this.gameBoard[1][i] === 1 && this.gameBoard[2][i] === 1) {
+        return "Player 1 wins";
       }
     }
-
-    resetGame() {
-      this.totalMoves = 0;
-      this.turn = this.player1;
-      this.gameBoard = [0,0,0
-                        0,0,0
-                        0,0,0];
+    if (this.gameBoard[0][0] === 1 && this.gameBoard[1][1] === 1 && this.gameBoard[2][2] === 1) {
+      return "Player 1 wins";
+    } else if (this.gameBoard[0][2] === 1 && this.gameBoard[1][1] === 1 && this.gameBoard[2][0] === 1) {
+      return "Player 1 wins";
     }
+  }
+
+  checkWinPlayer2() {
+    for (var i = 0; i < this.gameBoard.length; i++) {
+      if (this.gameBoard[i][0] === 2 && this.gameBoard[i][1] === 2 && this.gameBoard[i][2] === 2) {
+        return "Player 2 wins";
+      } else if (this.gameBoard[0][i] === 2 && this.gameBoard[1][i] === 2 && this.gameBoard[2][i] === 2) {
+        return "Player 2 wins";
+      }
+    }
+    if (this.gameBoard[0][0] === 2 && this.gameBoard[1][1] === 2 && this.gameBoard[2][2] === 2) {
+      return "Player 2 wins";
+    } else if (this.gameBoard[0][2] === 2 && this.gameBoard[1][1] === 2 && this.gameBoard[2][0] === 2) {
+      return "Player 2 wins";
+    }
+  }
+  checkCatsGame() {
+    if (this.totalMoves >= 9) {
+      return "Cats game";
+    }
+  }
+
+  resetGame() {
+    this.totalMoves = 0;
+    this.turn = this.player1;
+    this.gameBoard = [
+      [0, 0, 0]
+      [0, 0, 0]
+      [0, 0, 0]
+    ];
+  }
 }
